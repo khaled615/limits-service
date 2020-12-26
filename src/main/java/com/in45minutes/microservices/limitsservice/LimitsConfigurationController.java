b0418834efdc14e163e848bc9c1e7c6a2dea0c18
khaled615 <khaled.benhafsia07@gmail.com>
2020-12-26 15:03:03 +0100

    Service Limites

---
+++ src/main/java/com/in45minutes/microservices/limitsservice/LimitsConfigurationController.java
@@ -0,0 +1,26 @@
+package com.in45minutes.microservices.limitsservice;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.in45minutes.microservices.limitsservice.bean.LimitConfiguration;
+
+@RestController
+public class LimitsConfigurationController {
+	
+	@Autowired
+	private Configuration configuration;
+	
+	@GetMapping("/limits") 
+	public LimitConfiguration retrieveLimitsFromConfiguration() {
+		//return new LimitConfiguration(1000,1);
+		return new LimitConfiguration(configuration.getMaximum(),configuration.getMinimum());
+	}
+
+	
+	
+	
+	
+	
+}
